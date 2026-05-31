95111c87c7cd2f4c21f0f5c363ba7b6df6e2ac8e
Lass1200 <126900557+Lass1200@users.noreply.github.com>
2026-05-31 22:34:48 +0200

    Update serverSlice.tsx

--- src/store/serverSlice.tsx
+++ src/store/serverSlice.tsx
@@ -1,16 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { ServerState } from '../../types'
-
+// State initial — IP et port vides, non connecté au démarrage
 const initialState: ServerState = {
     ip: '',
     port: '',
     isConnected: false
 }
-
+// ce slice gere donc la connexion au serveur python
 const serverSlice = createSlice({
     name: 'server',
     initialState,
     reducers: {
+         // Met à jour l'adresse IP saisie par l'utilisateur
         setIp: (state, action: PayloadAction<string>) => {
             state.ip = action.payload
         },
